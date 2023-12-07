8f4edf1b88e2d1082da647122e63259a3a1f1444
Jacob Padley <jacob5180@hotmail.co.uk>
2023-12-07 09:57:49 +0000

    Add solutions for Day 7

---
+++ day7/part1.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+use v5.35;
+
+open(my $input, "<", "input.txt") or die "Can't open puzzle input: $!";
+
+my @hand_type_groups;
+my $num_cards = 0;
+while(<$input>) {
+    if(/^(.{5})\s+(\d+)/) {
+        my $hand = $1;
+        my $score = $2;
+        $num_cards++;
+        my %counts;
+        foreach my $char (split(//, $hand)) {
+            $counts{$char} = length( $hand =~ s/[^\Q$char\E]//rg ) if !exists($counts{$char});
+        }
+        # Find the highest counts
+        my @ordered = sort { $counts{$a} <=> $counts{$b} } keys %counts;
+        my $highest = $ordered[-1];
+
+        if($counts{$highest} == 5) {
+            push @{$hand_type_groups[0]}, [$hand, $score];
+        } elsif($counts{$highest} == 4) {
+            push @{$hand_type_groups[1]}, [$hand, $score];
+        } elsif($counts{$highest} == 3) {
+            if($counts{$ordered[-2]} == 2) {
+                push @{$hand_type_groups[2]}, [$hand, $score];
+            } else {
+                push @{$hand_type_groups[3]}, [$hand, $score];
+            }
+        } elsif($counts{$highest} == 2) {
+            if($counts{$ordered[-2]} == 2) {
+                push @{$hand_type_groups[4]}, [$hand, $score];
+            } else {
+                push @{$hand_type_groups[5]}, [$hand, $score];
+            }
+        } else {
+            push @{$hand_type_groups[6]}, [$hand, $score];
+        }
+    }
+}
+
+my $total = 0;
+my $rank = $num_cards;
+foreach my $hand_group (@hand_type_groups) {
+    foreach my $hand_score (sort { encode(@{$a}[0]) cmp encode(@{$b}[0]) } @{$hand_group}) {
+        my $score = @{$hand_score}[1];
+        $total += $score * $rank;
+        $rank--;
+    }
+}
+
+print "$total\n";
+
+# I love regexes
+sub encode {
+    my $str = shift;
+    $str =~ tr/AKQJT987654321/A-N/;
+    return $str;
+}
+
+close $input or die "$input: $!";
